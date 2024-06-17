b2481b74222ebf558db5b47cd21ef09fb13b008b
LongNguyen <nguyenhoanglong112001@gmail.com>
2024-06-17 14:50:02 +0700

    commit thêm

--- Assets/_Asset/Script/PlayerScript/GroundCheck.cs
+++ Assets/_Asset/Script/PlayerScript/GroundCheck.cs
@@ -8,30 +8,6 @@ public class GroundCheck : MonoBehaviour
     [SerializeField] private LayerMask layer;
     [SerializeField] private Transform groundcheck;
     [SerializeField] private float radius;
-    //[SerializeField] private Animator anima;
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.CompareTag("Ground") || collision.CompareTag("UpGround"))
-    //    {
-    //        if(anima.runtimeAnimatorController != null)
-    //        {
-    //            anima.SetBool("IsGround", true);
-    //            anima.SetBool("IsWalling", false);
-    //        }
-    //    }
-    //}
-
-    //private void OnTriggerExit2D(Collider2D collision)
-    //{
-    //    if (collision.CompareTag("Ground") || collision.CompareTag("UpGround"))
-    //    {
-    //        anima.SetBool("IsGround", false);
-    //    }
-    //    if (collision.CompareTag("DownGround"))
-    //    {
-    //        anima.SetBool("IsDown", false);
-    //    }
-    //}
 
     private void Update()
     {
